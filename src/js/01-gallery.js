88e653b01899b083f600810fb167d5e624bc594e
Piretrum <129379218+Piretrum@users.noreply.github.com>
2023-10-04 21:22:55 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
+
 console.log(galleryItems);
 
-const xxx = 1;
-console.log(xxx);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+    .map(
+        ({preview, original, description}) => 
+`<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img 
+      class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+   captionsData: 'alt',
+   captionPosition: 'bottom',
+   captionDelay: 250,
+ });
